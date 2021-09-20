a39a16dd8f641114b9f925f26639730358bee4f2
MrMallIronmaker <725mrm@gmail.com>
2021-09-20 16:37:29 -0700

    heroku 8 base dir correctw

--- paug/settings.py
+++ paug/settings.py
@@ -123,7 +123,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'paug/paugapp/static'),
+    # os.path.join(BASE_DIR, 'paug/paugapp/static'),
     os.path.join(BASE_DIR, 'paugapp/static')
 )
 
