35b9585401ce73e398b05f7c5fa3f21c61c45cac
MrMallIronmaker <725mrm@gmail.com>
2021-09-20 16:39:00 -0700

    heroku 9 missing a comma

--- paug/settings.py
+++ paug/settings.py
@@ -124,7 +124,7 @@ STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     # os.path.join(BASE_DIR, 'paug/paugapp/static'),
-    os.path.join(BASE_DIR, 'paugapp/static')
+    os.path.join(BASE_DIR, 'paugapp/static'),
 )
 
 # Default primary key field type
